466e33ab276fa6d5231cff4299a0da26d897573f
frankovlahov <Franko.Vlahov@playhera.com>
2024-10-07 15:46:05 +0200

    [ADD] ScreenPadding, Spacing handlers

---
+++ app/src/main/java/com/vlahov/fluencytask/ui/theme/ScreenPadding.kt
@@ -0,0 +1,6 @@
+package com.vlahov.fluencytask.ui.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.dp
+
+val LocalScreenPadding = staticCompositionLocalOf { 0.dp }
