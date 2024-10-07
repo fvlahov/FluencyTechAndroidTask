56e8d176dee2d89a77204e6c2b9c0e69fbc166da
frankovlahov <Franko.Vlahov@playhera.com>
2024-10-07 15:35:47 +0200

    [ADD] BaseScreen composable

---
+++ app/src/main/java/com/vlahov/fluencytask/ui/layout/BaseScreen.kt
@@ -0,0 +1,40 @@
+package com.vlahov.fluencytask.ui.layout
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BaseScreen(
+    modifier: Modifier = Modifier,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    topBar: @Composable () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
+    background: @Composable () -> Unit = {},
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Scaffold(
+        topBar = topBar,
+        containerColor = MaterialTheme.colorScheme.background,
+        floatingActionButton = floatingActionButton,
+    ) { innerPadding ->
+        background()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .then(modifier),
+            horizontalAlignment = horizontalAlignment,
+            verticalArrangement = verticalArrangement,
+            content = content
+        )
+    }
+}
