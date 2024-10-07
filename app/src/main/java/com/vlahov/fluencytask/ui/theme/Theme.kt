e21710269d26d9604eb0f4bdbf25b4195b423083
frankovlahov <Franko.Vlahov@playhera.com>
2024-10-07 21:05:31 +0200

    [UPDATE] Use only light theme

--- app/src/main/java/com/vlahov/fluencytask/ui/theme/Theme.kt
+++ app/src/main/java/com/vlahov/fluencytask/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.vlahov.fluencytask.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -32,20 +31,14 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun FluencyTaskTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     CompositionLocalProvider(
         LocalSpacing provides 16.dp,
         LocalScreenPadding provides 32.dp
     ) {
         MaterialTheme(
-            colorScheme = colorScheme,
+            colorScheme = LightColorScheme,
             typography = Typography,
             content = content
         )
